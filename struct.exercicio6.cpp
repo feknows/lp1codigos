94200e9608bd6ded1bd794adc1dd88b47e532a37
Felipe Alves <fapalmeida18@gmail.com>
2017-04-04 22:38:40 -0300

    Update struct.exercicio6.cpp

--- struct.exercicio6.cpp
+++ struct.exercicio6.cpp
@@ -41,7 +41,7 @@ struct obs{
   char obs[150];
 };
 
-struct pessoa{
+struct agenda{
   int codigo;
   char nome[50];
   char email[50];
@@ -52,16 +52,64 @@ struct pessoa{
 };
 
 main(){
-  pessoa p[100];
+  agenda ag[100];
   int cont;
   for(cont=0; cont<100; cont++){
       fflush(stdin);
       printf("\nCodigo: ");
-      scanf("%d", &p[cont].codigo);
-      
+      scanf("%d", &ag[cont].codigo);
+      fflush(stdin);
+      printf("\nNome: ");
+      gets(ag[cont].nome);
+      printf("\nEmail: ");
+      gets(ag[cont].email);
+      fflush(stdin);
+      printf("\nDDD do Telefone: ");
+      scanf("%d", &ag[cont].tel.ddd);
+      printf("\nNumero do Telefone: ");
+      scanf("%d", &ag[cont].tel.numero);
+      printf("\nDia do Aniversario: ");
+      scanf("%d", &ag[cont].dataAniversario.dia);
+      printf("\nMes do Aniversario: ");
+      scanf("%d", &ag[cont].dataAniversario.mes);
+      printf("\nAno do Aniversario: ");
+      scanf("%d", &ag[cont].dataAniversario.ano);
+      fflush(stdin);
+      printf("\nEndereço");
+      printf("\nRua: ");
+      gets(ag[cont].end.rua);
+      fflush(stdin);
+      printf("\Numero: ");
+      scanf("%d", &ag[cont].end.numero);
+      fflush(stdin);
+      printf("\Complemento: ");
+      gets(ag[cont].end.complemento);
+      fflush(stdin);
+      printf("\nBairro: ");
+      gets(ag[cont].end.bairro);
+      fflush(stdin); 
+      printf("\CEP: ");
+      scanf("%d", &ag[cont].end.cep);
+      fflush(stdin);
+      printf("\Cidade: ");
+      gets(ag[cont].end.cidade);
+      printf("\Estado: ");
+      gets(ag[cont].end.estado);
+      printf("\Pais: ");
+      gets(ag[cont].end.pais);    
+      printf("\Observacoes: ");
+      gets(ag[cont].obsrv.obs)
   }
   
-  
+  for(cont=0; cont<100; cont++){
+    printf("\n --- AGENDA --- ");
+    printf("\n CODIGO: %d - NOME: %s - E-MAIL: %s \n -- ENDEREÇO: RUA: %s, %d - %s - , %s \nCEP: %d - %s - %s - %s \nObservacoes: %s \nTelefone: %d - %d \n ANIVERSARIO: %d/%d/%d \n OBSERVACOES: %s", 
+           
+           ag[cont].codigo, ag[cont].nome, ag[cont].email, ag[cont].end.rua, ag[cont].end.numero, ag[cont].end.complemento,
+           ag[cont].end.bairro, ag[cont].end.cep, ag[cont].end.cidade, ag[cont].end.estado, ag[cont].end.pais, ag[cont].end.complemento,
+           ag[cont].tel.ddd, ag[cont].tel.numero, ag[cont].dataAniversario.dia, ag[cont].dataAniversario.mes, ag[cont].dataAniversario.ano,
+           ag[cont].obsrv.obs );
+  }
   
   
   
